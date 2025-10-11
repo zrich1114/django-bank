830942953e5c3be581d9ae42fbd4fc5c13a3758c
Zach Richardson <zachrichardson1114@gmail.com>
2025-10-11 12:39:54 -0600

    initial commit

---
+++ config/settings/local.py
@@ -0,0 +1,27 @@
+from os import getenv, path
+from dotenv import load_dotenv
+from .base import *  # noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+DEBUG = getenv("DEBUG")
+
+SITE_NAME = getenv("SITE_NAME")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
---
+++ core_apps/user_auth/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class UserAuthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core_apps.user_auth"
+    verbose_name = _("User Auth")
