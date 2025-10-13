fe2839ed3d7255f5b6eb247b0872741b76f17938
Zach Richardson <zachrichardson1114@gmail.com>
2025-10-13 10:18:28 -0600

    feat: customized user admin view

--- core_apps/user_auth/admin.py
+++ core_apps/user_auth/admin.py
@@ -1,3 +1,88 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+  form = UserChangeForm
+  add_form = UserCreationForm
+  model = User
+  list_display = [
+    'email',
+    'username',
+    'first_name',
+    'last_name',
+    'is_staff',
+    'is_active',
+    'role',
+  ]
+  list_filter = ['email', 'is_staff', 'is_active', 'role']
+  fieldsets = (
+    (
+      _('Login Credentials'),
+      {
+        'fields': (
+          'username',
+          'email',
+          'password',
+        )
+      },
+    ),
+    (
+      _('Personal Information'),
+      {
+        'fields': (
+          'first_name',
+          'middle_name',
+          'last_name',
+          'id_no',
+          'role',
+        )
+      },
+    ),
+    (
+      _('Account Status'),
+      {
+        'fields': (
+          'account_status',
+          'failed_login_attempts',
+          'last_failed_login',
+        )
+      },
+    ),
+    (
+      _('Security'),
+      {
+        'fields': (
+          'security_question',
+          'security_answer',
+        )
+      },
+    ),
+    (
+      _('Permissions and Groups'),
+      {
+        'fields': (
+          'is_active',
+          'is_staff',
+          'is_superuser',
+          'groups',
+          'user_permissions',
+        )
+      },
+    ),
+    (
+      _('Important Dates'),
+      {
+        'fields': (
+          'last_login',
+          'date_joined',
+        )
+      },
+    ),
+  )
+  search_fields = ['email', 'username', 'first_name', 'last_name']
+  ordering = ['email']
