b5c3d7bc67a15cf1612ad22f58809fc5e81a20bf
Zach Richardson <zachrichardson1114@gmail.com>
2025-10-13 10:30:26 -0600

    feat: added drf_spectacular settings and urls

--- config/settings/base.py
+++ config/settings/base.py
@@ -149,6 +149,21 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'user_app.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'NextGen Bank API',
+    'DESCRIPTION': 'An API built for a banking system',
+    'VERSION': "1.0.0",
+    'SERVE_INCLUDE_SCHEMA': False,
+    'LICENSE': {
+        'name': 'MIT License',
+        'url': 'https://opensource.org/license/MIT', 
+    }
+}
+
 LOGGING_CONFIG = None
 
 LOGURU_LOGGING = {
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,19 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+  SpectacularAPIView,
+  SpectacularRedocView,
+  SpectacularSwaggerView
+)
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('/api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('/api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+admin.site.site_header = 'NextGen Bank Admin'
+admin.site.site_title = 'NextGen Bank Admin Portal'
+admin.site.index_title = 'Welcome to NextGen Bank Admin Portal'
