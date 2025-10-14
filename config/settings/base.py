abaf48e10f96c1952e6fbfa7a38eb71bcf0ff859
Zach Richardson <zachrichardson1114@gmail.com>
2025-10-14 07:06:25 -0600

    feat: Configured nginx

--- config/settings/base.py
+++ config/settings/base.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core_apps.user_auth.middleware.CustomHeaderMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
