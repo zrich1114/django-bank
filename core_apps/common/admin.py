281a9f886c12c63dea8764d412bf80740030133c
Zach Richardson <zachrichardson1114@gmail.com>
2025-10-15 07:28:55 -0600

    feat: Added TimeStamped Model and ContentView Model with admin view

--- core_apps/common/admin.py
+++ core_apps/common/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from typing import Any
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.http import HttpRequest
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import ContentView
+
+@admin.register(ContentView)
+class ContentViewAdmin(admin.ModelAdmin):
+  list_display = [
+    'content_object',
+    'content_type',
+    'user',
+    'viewer_ip',
+    'last_viewed',
+    'created_at',
+  ]
+  list_filter = ['content_type', 'last_viewed', 'created_at']
+  date_hierarchy = 'last_viewed'
+  readonly_fields = [
+    'content_type',
+    'object_id',
+    'content_object',
+    'user',
+    'viewer_ip',
+    'created_at',
+    'updated_at',
+  ]
+  fieldsets = (
+    (None, {
+      'fields': (
+        'content_type',
+        'object_id',
+        'content_object'
+      )
+    }),
+    (_('View Details'), {
+      'fields': (
+        'user',
+        'viewer_ip',
+        'last_viewed'
+      )
+    }),
+    (_('Timestamps'), {
+      'fields': (
+        'created_at',
+        'updated_at'
+      ),
+      'classes': (
+        'collapse',
+      )
+    }),
+  )
+
+  def has_add_permission(self, request: HttpRequest) -> bool:
+    return False
+  
+  def has_change_permission(self, request: HttpRequest, obj: Any = None) -> bool:
+    return False
+
+class ContentViewInline(GenericTabularInline):
+  model = ContentView
+  extra = 0
+  readonly_fields = [
+    'user',
+    'viewer_ip',
+    'last_viewed',
+    'created_at',
+  ]
+  can_delete = False
+
+  def has_add_permission(self, request: HttpRequest, obj: Any = None) -> bool:
+    return False
+  
--- core_apps/common/models.py
+++ core_apps/common/models.py
@@ -1,3 +1,69 @@
-from django.db import models
+import uuid
+from typing import Any, Optional
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import IntegrityError, models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+class TimeStampedModel(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    abstract = True
+
+class ContentView(TimeStampedModel):
+  content_type = models.ForeignKey(
+    ContentType, 
+    on_delete=models.CASCADE, 
+    verbose_name=_('Content Type')
+  )
+  object_id = models.UUIDField(verbose_name=_('Object ID'))
+  content_object = GenericForeignKey('content_type', 'object_id')
+  user = models.ForeignKey(
+    User, 
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name='content_views',
+    verbose_name=_('User')
+  )
+  viewer_ip = models.GenericIPAddressField(
+    verbose_name=_('Viewer IP Address'),
+    null=True,
+    blank=True
+  )
+  last_viewed = models.DateTimeField()
+
+  class Meta:
+    verbose_name = _('Content View')
+    verbose_name_plural = _('Content Views')
+    unique_together = ['content_type', 'object_id', 'user', 'viewer_ip']
+
+    def __str__(self):
+      return f'{self.content_type} viewed by {self.user.get_full_name if self.user else "Anonymous"} from IP {self.viewer_ip}'
+    
+    @classmethod
+    def record_view(cls, content_object: Any, user: Optional[User], viewer_ip: str) -> None:
+      content_type = ContentType.objects.get_for_model(content_object)
+      try:
+        view, created = cls.objects.get_or_created(
+          content_type=content_type,
+          object_id=content_object.id,
+          defaults={
+            'user': user,
+            'viewer_ip': viewer_ip,
+            'last_viewed': timezone.now()
+          },
+        )
+        if not created:
+          view.last_viewed = timezone.now()
+          view.save()
+      except IntegrityError:
+        pass
+
